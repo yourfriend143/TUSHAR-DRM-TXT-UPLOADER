025ef9c0fbbfb39f48ff32e995453b92d8cb856e
Tushar <tusartxt7570@gmail.com>
2025-05-30 17:38:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ OWNER_ID = 5840594311 # Replace with the actual owner's user ID
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [5840594311]
 
-AUTH_CHANNEL = -1002572301679
+AUTH_CHANNEL = -1002595188554
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
